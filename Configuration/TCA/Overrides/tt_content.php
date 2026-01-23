0c80a8b922ff77f5321027c795bcf936021eb388
Ernedin Zajko <ezajko@root.ba>
2026-01-23 06:02:49 +0100

    refactor: register plugins as CType with Wizard group

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -8,34 +8,88 @@
 
 defined('TYPO3') or die();
 
-// Register Pi1: List / Download
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'SparkDms',
-    'Pi1',
-    'Spark DMS: List / Download',
-    'spark-dms-module'
+// --- New CType Registration (Documents Group) ---
+
+// 1. List / Download
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    [
+        'Spark DMS: List / Download',
+        'sparkdms_list',
+        'EXT:spark_dms/Resources/Public/Icons/Extension.svg'
+    ],
+    'CType',
+    'spark_dms'
 );
 
-// Register Pi2: Detail View
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'SparkDms',
-    'Pi2',
-    'Spark DMS: Detail View',
-    'spark-dms-module'
-);
+// Configure ShowItem for List
+$GLOBALS['TCA']['tt_content']['types']['sparkdms_list'] = [
+    'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            --palette--;;headers,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,
+            pi_flexform,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+            --palette--;;frames,
+            --palette--;;appearanceLinks,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+            categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+    '
+];
 
-// Add FlexForm for Pi1 (List)
-$pluginSignaturePi1 = 'sparkdms_pi1';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignaturePi1] = 'pi_flexform';
+// Add FlexForm for List (Reuse existing)
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    $pluginSignaturePi1,
-    'FILE:EXT:spark_dms/Configuration/FlexForms/List.xml'
+    '*',
+    'FILE:EXT:spark_dms/Configuration/FlexForms/List.xml',
+    'sparkdms_list'
 );
 
-// Add FlexForm for Pi2 (Detail)
-$pluginSignaturePi2 = 'sparkdms_pi2';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignaturePi2] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    $pluginSignaturePi2,
-    'FILE:EXT:spark_dms/Configuration/FlexForms/Detail.xml'
+
+// 2. Detail View
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    [
+        'Spark DMS: Detail View',
+        'sparkdms_detail',
+        'EXT:spark_dms/Resources/Public/Icons/Extension.svg'
+    ],
+    'CType',
+    'spark_dms'
 );
+
+// Configure ShowItem for Detail
+$GLOBALS['TCA']['tt_content']['types']['sparkdms_detail'] = [
+    'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            --palette--;;headers,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,
+            pi_flexform,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+            --palette--;;frames,
+            --palette--;;appearanceLinks,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+            categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+    '
+];
+
+// Add FlexForm for Detail (Reuse existing)
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    '*',
+    'FILE:EXT:spark_dms/Configuration/FlexForms/Detail.xml',
+    'sparkdms_detail'
+);
+
--- ext_localconf.php
+++ ext_localconf.php
@@ -33,4 +33,17 @@ defined('TYPO3') or die();
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['spark_dms'] 
         = \RootBa\SparkDms\Hook\DataHandlerHook::class;
 
+    // Register Icons
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'ext-spark_dms-wizard-list',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:spark_dms/Resources/Public/Icons/Extension.svg']
+    );
+    $iconRegistry->registerIcon(
+        'ext-spark_dms-wizard-detail',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:spark_dms/Resources/Public/Icons/Document.svg']
+    );
+
 })();
